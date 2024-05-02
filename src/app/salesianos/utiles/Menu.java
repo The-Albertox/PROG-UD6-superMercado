a994ad0d1cc3b289c016f9282c1f011448e9cb85
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-05-02 08:43:25 +0100

    refactor(menu): delete part of the mensage that was unneccesary

--- src/app/salesianos/utiles/Menu.java
+++ src/app/salesianos/utiles/Menu.java
@@ -1,16 +1,15 @@
 package app.salesianos.utiles;
 
 public class Menu {
-    public String requestMenuOption() {
+    public String getMenu() {
         String menu = """
-            Menú de opciones:
-            1. Abrir caja.
-            2. Añadir nuevo cliente a la cola.
-            3. Atender un cliente.
-            4. Ver clientes pendientes.
-            5. Cerrar supermercado.
-            Elija una opción: 
-                """;
-       return menu;
+                Menú de opciones:
+                1. Abrir caja.
+                2. Añadir nuevo cliente a la cola.
+                3. Atender un cliente.
+                4. Ver clientes pendientes.
+                5. Cerrar supermercado.
+                    """;
+        return menu;
     }
 }
